2f55d7f34489befc89950b956ff0b408ff400fc9
Pepe Gombos <peter@kriek.hu>
2015-09-11 14:41:45 +0200

    initials only addresses are now created

--- index.js
+++ index.js
@@ -4,21 +4,29 @@ var _ = require('lodash');
 createPossibleAddresses = function(name, domain){
 	var addresses = [];
 	var variations = [];
+
+	// Clean domain input
 	domain = domain.trim().toLowerCase();
 
+	// Removing non english characters and converting name to lovercase
 	var latinize = require('latinize');
 	name = latinize(name.toLowerCase());
 	
+	// Converting name string to an array
 	var nameParts = [];
 	nameParts[0] = name.trim().replace(/[^a-zA-Z ]/g, "").split(" ");
 
+	// Conuter for variations
 	var j = 0;
 
+	// Combinations library
 	var Combinatorics = require('js-combinatorics');
 
+	// Execute cominations on simpla name parts
 	cmb = Combinatorics.permutationCombination(nameParts[0]);
 	variations[0] = cmb.toArray();
 
+	// Creating initials with normal name parts
 	for(var i = 0;i < nameParts[0].length;i++){
 		j++;
 		nameParts[j] = _.clone(nameParts[0]);
@@ -27,8 +35,21 @@ createPossibleAddresses = function(name, domain){
 		variations[j] = cmb.toArray();
 	}
 
+	// Creating initials only nameparts
+	j++;
+	nameParts[j] = _.clone(nameParts[0]);
+	for(var i = 0;i < nameParts[0].length;i++){
+		nameParts[j][i] = nameParts[j][i].substring(0,1);
+	}
+	cmb = Combinatorics.permutationCombination(nameParts[j]);
+	variations[j] = cmb.toArray();
+
+
+	// Flattening array
 	variations = _.flatten(variations);
 
+
+	// Creating final addresses from variations
 	for(var i=0; i < variations.length; i++){
 		addresses.push(variations[i].join('') + "@" + domain);
 		addresses.push(variations[i].join('.') + "@" + domain);
