d9e61e09e962274b3b218fee561a93b538800815
Peter G <peter@kriek.hu>
2015-11-01 18:27:53 +0100

    remote server hangup now calls callback with error

--- email-existence.js
+++ email-existence.js
@@ -10,16 +10,16 @@ var dns = require('dns'),
  * This module is based on: https://github.com/nmanousos/email-existence
  */
 module.exports = function(accounts, domain, options, callback) {
-	
+
 	// check if options is given
-	if(typeof options === "function"){
+	if (typeof options === "function") {
 		callback = options;
 		options = {};
 	}
 
 
 	// check if only one email is given
-	if(typeof accounts === "string"){
+	if (typeof accounts === "string") {
 		accounts = [accounts];
 	}
 
@@ -28,56 +28,62 @@ module.exports = function(accounts, domain, options, callback) {
 
 	// Create and validate email addresses from accouns
 	var emails = [];
-	for(var i = 0;i < accounts.length; i++){
+	for (var i = 0; i < accounts.length; i++) {
 		var email = accounts[i] + "@" + domain;
 		if (/^\S+@\S+$/.test(email)) {
 			emails.push(email);
 		}
 	}
-	
+
 	// If not even one email address is given retun an error
-	if(emails.length === 0){
-		return callback({code : 700, message : "Could not create valid email addresses from accounts"}, validAddesses);
+	if (emails.length === 0) {
+		return callback({
+			code: 700,
+			message: "Could not create valid email addresses from accounts"
+		}, validAddesses);
 	}
-	
+
 	// Set defaults
 	options.timeout = options.timeout || 10000;
 	options.from_email = options.from_email || emails[0];
 	options.debug = options.debug || false;
-	
+
 
 	// Getting the MX records for the domain
 	dns.resolveMx(domain, function(err, addresses) {
-		
+
 		// If there is anything wrong with the domain or no MX record found
 		if (err || addresses.length === 0) {
-			callback({code:701, message : "Could not found valid MX records for this domain: " + domain}, validAddesses);
+			callback({
+				code: 701,
+				message: "Could not found valid MX records for this domain: " + domain
+			}, validAddesses);
 			return;
 		}
-		
+
 
 		// Creating socket
 		var conn = net.createConnection(25, addresses[0].exchange);
-		
+
 		// This is wehere we keep the validated addresses
 		var validAddesses = [];
-		
+
 
 		// This is an invalid email address we use to detect catch all domains
 		var invalidEmail = Math.random().toString(36).substring(7) + emails[0];
-		
+
 		// A set of commands for the initial connection
-		var commands = ["EHLO " + ( options.fqdn || addresses[0].exchange), "MAIL FROM:<" + options.from_email + ">", "RCPT TO:<" + invalidEmail + ">"];
-		
+		var commands = ["EHLO " + (options.fqdn || addresses[0].exchange), "MAIL FROM:<" + options.from_email + ">", "RCPT TO:<" + invalidEmail + ">"];
+
 
 		// Adding email addresses to the commands
-		for(var i = 0;i < emails.length; i++){
+		for (var i = 0; i < emails.length; i++) {
 			commands.push("RCPT TO:<" + emails[i] + ">");
 		}
 
 		// At last we set the last QUIT command to cleanly close connections
 		commands.push("QUIT");
-		
+
 		// We store the our current command index in this variable
 		var i = 0;
 
@@ -86,10 +92,13 @@ module.exports = function(accounts, domain, options, callback) {
 
 		// Setting timeout
 		conn.setTimeout(options.timeout);
-		
+
 		// On error, we close the connection
 		conn.on('error', function() {
-			conn.emit('false',{code : 702, message : "Connection error"});
+			conn.emit('false', {
+				code: 702,
+				message: "Connection error"
+			});
 		});
 
 		// If anything goes bad this gets called
@@ -101,7 +110,18 @@ module.exports = function(accounts, domain, options, callback) {
 
 		// Gets called on connection timeout
 		conn.on('timeout', function() {
-			conn.emit('false', {code:703, message :"Connection timeout"});
+			conn.emit('false', {
+				code: 703,
+				message: "Connection timeout"
+			});
+		});
+
+		// Server hang-up on us 
+		conn.on('end', function() {
+			callback({
+				code: 706,
+				message: "Connection has been terminated by the server"
+			}, validAddesses);
 		});
 
 		// On Connection
@@ -109,7 +129,7 @@ module.exports = function(accounts, domain, options, callback) {
 			// Ready for the next command to be sent
 			conn.on('prompt', function() {
 				if (i < commands.length) {
-					if(options.debug){
+					if (options.debug) {
 						console.log(commands[i]);
 					}
 					conn.write(commands[i]);
@@ -129,42 +149,49 @@ module.exports = function(accounts, domain, options, callback) {
 
 				// Removing line breaks
 				var response = data.toString().replace(/(?:\\[rn]|[\r\n]+)+/g, "");
-				
+
 				// Grepping the response code
-				var responseCode = parseInt(response.substring(0,3),10);
-				
+				var responseCode = parseInt(response.substring(0, 3), 10);
+
 				// Grepping subcode
-				var responseSubCode = response.substring(4,9);
-				
+				var responseSubCode = response.substring(4, 9);
+
 				// Console log response if debug is set true
-				if(options.debug){
+				if (options.debug) {
 					console.log(response);
 				}
-				
+
 				// If response has positive codes
-				if (responseCode === 220 || responseCode === 250 || responseCode === 221 || responseCode === 450){
-					
+				if (responseCode === 220 || responseCode === 250 || responseCode === 221 || responseCode === 450) {
+
 					// If this response is after the catchall check we retun an error
 					if (i === 3) {
-						conn.emit('false', {code: 705, message : "Domain has catchall address"});
+						conn.emit('false', {
+							code: 705,
+							message: "Domain has catchall address"
+						});
 					} else {
 
 						// Woohoo new validated address found  
 						// (450) is for google apps https://support.google.com/mail/answer/6592
-						if(i > 3 && (responseCode === 250 || responseCode === 450)){
-							validAddesses.push(emails[i-4]);
+						if (i > 3 && (responseCode === 250 || responseCode === 450)) {
+							validAddesses.push(emails[i - 4]);
 						}
-						
+
 						// Lets run the next command
 						conn.emit('prompt');
 
 					}
-				// If response contains 550, 553, 554 (email not found) lets go to the next command
+					// If response contains 550, 553, 554 (email not found) lets go to the next command
 				} else if (responseCode === 550 || responseCode === 553 || responseCode === 554) {
-							conn.emit('prompt');
-				// If anythimg else goes on retun it as an error
+					conn.emit('prompt');
+					// If anythimg else goes on retun it as an error
 				} else {
-						conn.emit('false', {code: 704, message : data.toString(), smtpStatusCode : responseCode});
+					conn.emit('false', {
+						code: 704,
+						message: data.toString(),
+						smtpStatusCode: responseCode
+					});
 				}
 			});
 		});
